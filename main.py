8dcc4880831bf988c943822ba84d0405d3d0260a
Selva Kumar B <selvakumarb@Selvas-MacBook-Air.local>
2026-09-09 16:29:26 +0530

    Added response format

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field 
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -7,6 +9,20 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage 
 from langchain_ollama import ChatOllama 
 from langchain_tavily import TavilySearch
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="Thr agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer"
+    )
 # from tavily import TavilyClient
 
 # tavily = TavilyClient()
@@ -26,7 +42,7 @@ from langchain_tavily import TavilySearch
 
 llm = ChatOllama(temperature=0, model="llama3.1")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
